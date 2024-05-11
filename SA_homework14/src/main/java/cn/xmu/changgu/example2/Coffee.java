d5c5f06dcbc31ef3a4225cd1cb707d60abb836ac
ChangGu <guchang0208@outlook.com>
2024-05-11 16:01:35 +0800

    homework14

---
+++ SA_homework14/src/main/java/cn/xmu/changgu/example2/Coffee.java
@@ -0,0 +1,15 @@
+package cn.xmu.changgu.example2;
+
+public abstract class Coffee {
+    CoffeeImp coffeeImp;
+
+    public void setCoffeeImp() {
+        this.coffeeImp = CoffeeImpSingleton.getTheCoffeeImp();
+    }
+
+    public CoffeeImp getCoffeeImp() {
+        return this.coffeeImp;
+    }
+
+    public abstract void pourCoffee();
+}
---
+++ SA_homework14/src/main/java/cn/xmu/changgu/example2/MilkCoffeeImp.java
@@ -0,0 +1,12 @@
+package cn.xmu.changgu.example2;
+
+//加奶
+public class MilkCoffeeImp extends CoffeeImp {
+    MilkCoffeeImp() {
+    }
+
+    public void pourCoffeeImp() {
+        System.out.println("加了美味的牛奶");
+    }
+}
+ 
